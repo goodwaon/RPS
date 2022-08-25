2b4a32b4f3b6cae412993f61b23c13e8d4648b78
Diego <DiegoVizcaino87@gmail.com>
2022-08-25 11:18:41 -0500

    Fixed HTML path

---
+++ RPS.js
@@ -0,0 +1,18 @@
+
+   function getComputerChoice() {
+
+    const compChoice = Math.floor(Math.random()) * 3;
+ 
+    console.log(compChoice);
+
+   }
+   
+   getComputerChoice();
+
+
+
+
+
+   //for (let i = 0; i < 5; i++) {
+    // your code here!
+ 
